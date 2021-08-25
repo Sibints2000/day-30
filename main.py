912b196ef20dcdd9b63c6ccf0cdde674c38f2659
Sibints2000 <sibints2000@gmail.com>
2021-08-25 11:11:44 +0530

    Ninth Commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # File not found
-# with open("a_file.txt") as file:
-#     file.read()
+try:
+    file = open("a_file.txt")
+except:
+    print("There was an error")
 
 # Key Error
 # a_dictionary = {"key: "value"}
